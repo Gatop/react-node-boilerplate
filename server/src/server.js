a8ca902a8db4abb944e02102d57a708458b29d3d
Jaime Gomez <gatomzl@gmail.com>
2019-04-10 14:58:01 -0500

    [Server] - Configuring docker on server side

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3500, function () {
+app.listen(4000, function () {
   console.log('Example app listening on port 3500!');
 });
