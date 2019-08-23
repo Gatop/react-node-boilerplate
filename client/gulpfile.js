3c31bffcca5b08de3dceb68a441d2902d8d38ec7
Jaime Gomez <gatomzl@gmail.com>
2019-08-22 22:18:13 -0500

    [Client] - Adding jslint to gulp watch

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,7 +2,8 @@
 
 const gulp = require('gulp'),
     sassLint = require('gulp-sass-lint'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    eslint = require('gulp-eslint');
 
 gulp.task('sass-lint', function () {
   return gulp.src('src/**/*.s+(a|c)ss')
@@ -13,7 +14,21 @@ gulp.task('sass-lint', function () {
     .pipe(sassLint.failOnError())
 });
 
+gulp.task('eslint', function() {
+  return gulp.src('src/**/*.js')
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
+});
+
 gulp.task('watch', function() {
   livereload.listen();
   gulp.watch('src/**/*.s+(a|c)ss', gulp.series('sass-lint'));
+  gulp.watch('src/**/*.js', gulp.series('eslint'));
 });
